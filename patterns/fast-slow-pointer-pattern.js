75899f93af8befa1ff436b9ceb720810218094db
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2024-02-09 01:09:29 +0500

    fast and slow exercises

--- patterns/fast-slow-pointer-pattern.js
+++ patterns/fast-slow-pointer-pattern.js
@@ -90,3 +90,93 @@ const foo = (arr, target) => {};
 const arr = [2, 4, 6, 3, 7];
 target = 9;
 //! circular array loop,duplicate numbers,isPalindrome
+
+//*============>> Circular Array <<=============//
+const IsCircularArray = (num) => {
+  const getNextIdx = (idx) => {
+    return (idx + num[idx] + num.length) % num.length;
+  };
+
+  for (let i = 0; i < num.length; i++) {
+    let slow = i;
+    let fast = i;
+
+    while (true) {
+      slow = getNextIdx(slow);
+      fast = getNextIdx(getNextIdx(fast));
+
+      if (slow === fast) {
+        if (slow === getNextIdx(slow)) {
+          break;
+        } else {
+          return true;
+        }
+      }
+
+      if (
+        num[slow] * num[getNextIdx(slow)] <= 0 ||
+        num[fast] * num[getNextIdx(fast)] <= 0
+      ) {
+        break;
+      }
+    }
+  }
+};
+
+// console.log(IsCircularArray([3, 1, 2]));
+
+//* =============>> Duplicate numbers <<============//
+function removeDuplicates(num) {
+  let obj = {};
+  let slow = 0;
+
+  for (let fast = 0; fast < num.length; fast++) {
+    if (!obj[num[fast]]) {
+      obj[num[fast]] = true;
+      if (slow !== fast) {
+        num[slow] = num[fast];
+      }
+      slow++;
+    }
+  }
+
+  return num.slice(0, slow);
+}
+
+let num = [1, 5, 2, 8, 9, 2, 5, 9, 7, 1, 3, 5, 8, 6, 7, 3, 2, 4];
+let res = removeDuplicates(num);
+// console.log(res);
+
+//* ===========>> IsPalindrome <<=========//
+const isPalindrome = (head) => {
+  let array = [];
+  let current = head;
+
+  while (current) {
+    array.push(current.value);
+    current = current.next;
+  }
+
+  let start = 0;
+  let end = array.length - 1;
+
+  while (start < end) {
+    if (array[start] !== array[end]) {
+      return false;
+    }
+
+    start++;
+    end--;
+  }
+
+  return true;
+};
+
+linkedList.Push(11);
+linkedList.Push(15);
+linkedList.Push(12);
+linkedList.Push(19);
+linkedList.Push(14);
+linkedList.Push(17);
+
+console.log(isPalindrome(linkedList.head));
