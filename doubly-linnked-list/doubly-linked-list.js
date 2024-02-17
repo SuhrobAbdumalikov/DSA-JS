3b2f29b1111d399d0177faf1793c839a1eaca35f
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2024-02-17 11:21:21 +0500

    doubly linkedlist

---
+++ doubly-linnked-list/doubly-linked-list.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  Push(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  Unshift(data) {
+    const newNode = new Node();
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.tail;
+      this.tail.prev = newNode;
+      this.tail = newNode;
+    }
+  }
+}
+
+const doublyLinkedList = new DoublyLinkedList();
+doublyLinkedList.Push("Hello world");
+doublyLinkedList.Push("Salom Dunyo");
+console.log(doublyLinkedList);
