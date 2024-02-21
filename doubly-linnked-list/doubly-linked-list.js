7db7a9b55f1b1bd2f8cd9867687149b7ceb80e24
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2024-02-21 11:58:28 +0500

    dubly linked list and fast and slow pointer exercises

--- doubly-linnked-list/doubly-linked-list.js
+++ doubly-linnked-list/doubly-linked-list.js
@@ -35,9 +35,46 @@ class DoublyLinkedList {
       this.tail = newNode;
     }
   }
+
+  Shift() {
+    let current = this.head;
+    this.head.next = current;
+    current = null;
+  }
 }
 
+const findLoopCircleWhereBegins = (head) => {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      slow = head;
+
+      while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next;
+      }
+
+      return slow;
+    }
+  }
+  return null;
+};
+
 const doublyLinkedList = new DoublyLinkedList();
-doublyLinkedList.Push("Hello world");
-doublyLinkedList.Push("Salom Dunyo");
-console.log(doublyLinkedList);
+
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
+head.next.next.next.next.next.next = new Node(7);
+head.next.next.next.next.next.next.next = new Node(8);
+head.next.next.next.next.next.next.next.next = new Node(9);
+head.next.next.next.next.next.next.next.next.next = head.next.next.next;
+// console.log(head);
